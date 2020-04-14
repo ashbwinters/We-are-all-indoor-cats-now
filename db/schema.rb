cacc68244ae22c110a3853f6cf0e8f727fd3b714
github username <github email address>
2020-04-14 16:24:11 -0600

    branch off and make a schema in order to start on seeds.rb for games

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_14_183456) do
+
+  create_table "games", force: :cascade do |t|
+    t.text "name"
+    t.integer "players_min"
+    t.integer "players_max"
+    t.integer "setup_time"
+    t.integer "play_time"
+    t.text "genre"
+    t.integer "age_min"
+    t.boolean "competative"
+    t.integer "learning_curve"
+    t.boolean "family_friendly"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.integer "num_players"
+    t.integer "youngest_age"
+    t.boolean "competative"
+    t.boolean "family_friendly"
+    t.integer "session_time"
+  end
+
+end
